a249ef8cd5cf6250f16d4d0b43a15421d98ef850
Brenhv <145620267+Brenhv@users.noreply.github.com>
2023-11-15 19:28:05 -0600

    Add files via upload

---
+++ Ejemplo2.py
@@ -0,0 +1,12 @@
+import datetime
+import time
+import pandas as pd
+import streamlit as st
+from PIL import Image
+
+#*Generación de la webapp*
+#Definir título
+st.title("Título: Analítica de Datos")
+#Definir header/subheader
+st.haeder("Este es un header")
+st.subheader("Este es un subheader")
